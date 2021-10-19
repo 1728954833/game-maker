ef2b2096ef5642363f2d30af9da3740abec91d71
1728954833 <1728954833@qq.com>
2021-10-19 22:14:32 +0800

    feat: 拖拽

--- src/component/Shape/index.tsx
+++ src/component/Shape/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import cn from 'classnames'
 import './index.less'
 
@@ -10,16 +10,58 @@ const Shape: React.FC<
     IShapeProps & React.HTMLAttributes<HTMLDivElement>
 > = (props) => {
     const { children, style } = props
+    const [show, setShow] = useState(false)
+    const shapeRef = useRef<HTMLDivElement>(null)
 
-    const renderPoints = () => {
-        const positionList = ['left-top', 'right-top', 'left-bottom', 'right-bottom']
-        return positionList.map(position => <div key={position} className={cn('point', `point-${position}`)}></div>)
+    const handleShapeMove = (e: React.MouseEvent) => {
+        e.preventDefault()
+
+        const el = shapeRef.current
+        if (!el || !show) return
+        const { clientX: x, clientY: y } = e
+        const { top, left } = el.getBoundingClientRect()
+        const move = (ev: MouseEvent) => {
+            const { clientX: mvX, clientY: mvY } = ev
+            // 324代表左边框的宽度
+            const posLeft = left + mvX - x - 324
+            // 64代表上边宽的宽度
+            const posTop = top + mvY - y - 64
+            el.style.left = `${posLeft}px`
+            el.style.top = `${posTop}px`
+        }
+
+        const up = () => {
+            document.removeEventListener('mousemove', move)
+            document.removeEventListener('mouseup', up)
+        }
+
+        document.addEventListener('mousemove', move)
+        document.addEventListener('mouseup', up)
     }
 
-    console.log(children)
+    const handleShowLine = () => {
+        setShow(!show)
+    }
+
+    const renderPoints = () => {
+        const renderList = ['left-top', 'right-top', 'left-bottom', 'right-bottom', 'line-top', 'line-left', 'line-right', 'line-bottom']
+        return renderList.map(item => {
+            if (item.includes('line')) {
+                return <div key={item} className={cn('line', item)}></div>
+            }
+            return <div key={item} className={cn('point', `point-${item}`)}></div>
+        })
+    }
+
     return (
-        <div className='Shape' style={style}>
-            {renderPoints()}
+        <div
+            ref={shapeRef}
+            className={cn('Shape', { show })}
+            style={style}
+            onDoubleClick={handleShowLine}
+            onMouseDown={handleShapeMove}
+        >
+            {show && renderPoints()}
             {children}
         </div>
     );
